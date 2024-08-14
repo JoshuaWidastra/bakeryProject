526abacd4b69c7a74bfb8e11875e40786015adc4
egahandayani <ega.handayani07@gmail.com>
2024-08-14 23:10:05 +0700

    Tambahan di views, public dan install boostrap

--- routes/users.js
+++ routes/users.js
@@ -1,16 +1,23 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
 
 // controller function. for later
-const { listUsers, registerUser, loginUser } = require('../controllers/userController');
+const {
+  listUsers,
+  registerUser,
+  loginUser,
+  showLanding,
+} = require("../controllers/userController");
+
+router.get("/", showLanding); // masih percobaan
 
 // route ke list all users
-router.get('/', listUsers);
+router.get("/", listUsers);
 
 // route ke register new user
-router.post('/register', registerUser);
+router.post("/register", registerUser);
 
 // route log in a user
-router.post('/login', loginUser);
+router.post("/login", loginUser);
 
 module.exports = router;
