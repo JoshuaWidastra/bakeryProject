60ea1f792525664b4663a5a9df5e90884e7826c5
egahandayani <ega.handayani07@gmail.com>
2024-08-15 18:02:37 +0700

    Perubahan Schema, Cart jadi Order History

--- controllers/cartController.js
+++ controllers/cartController.js
@@ -1,43 +1,59 @@
+const { where } = require("sequelize");
 const db = require("../models");
 
 // add item to cart
 const addToCart = async (req, res) => {
-  const productId = req.body.productId; 
-  const quantity = req.body.quantity || 1;
+  // const productId = req.body.productId;
+  // const quantity = req.body.quantity || 1;
 
-  if (req.session.userId) {
-    const userId = req.session.userId;
-    await db.CartItem.upsert({
-      userId: userId,
-      productId: productId,
-      quantity: quantity,
-    });
-  } else {
-    if (!req.session.cart) {
-      req.session.cart = [];
-    }
-    req.session.cart.push({ productId, quantity });
+  // if (req.session.userId) {
+  //   const userId = req.session.userId;
+  //   await db.CartItem.upsert({
+  //     userId: userId,
+  //     productId: productId,
+  //     quantity: quantity,
+  //   });
+  // } else {
+  //   if (!req.session.cart) {
+  //     req.session.cart = [];
+  //   }
+  //   req.session.cart.push({ productId, quantity });
+  // }
+
+  // res.redirect('/cart');
+  const { userId } = req.session;
+  const { id } = req.params;
+  try {
+    await db.Cart.create({ ProductId: id, UserId: 1 });
+    res.redirect(`/cart`);
+  } catch (err) {
+    console.log(err);
+    res.send(err);
   }
-
-  res.redirect('/cart');
 };
 
 // vview cart items
 const viewCart = async (req, res) => {
-  let cartItems = [];
-  if (req.session.userId) {
-    cartItems = await db.CartItem.findAll({
-      where: { userId: req.session.userId },
-      include: [db.Product],
-    });
-  } else if (req.session.cart) {
-    cartItems = await Promise.all(req.session.cart.map(async (item) => {
-      const product = await db.Product.findByPk(item.productId);
-      return { ...item, product };
-    }));
-  }
-  
-  res.render('Cart', { cartItems });
+  // let cartItems = [];
+  // if (req.session.userId) {
+  //   cartItems = await db.CartItem.findAll({
+  //     where: { userId: req.session.userId },
+  //     include: [db.Product],
+  //   });
+  // } else if (req.session.cart) {
+  //   cartItems = await Promise.all(
+  //     req.session.cart.map(async (item) => {
+  //       const product = await db.Product.findByPk(item.productId);
+  //       return { ...item, product };
+  //     })
+  //   );
+  // }
+  const cartItems = await db.Cart.findAll({
+    where: { UserId: req.session.userId },
+    include: [db.Product],
+  });
+
+  res.render("Cart", { cartItems });
 };
 
 // remove item from cart
@@ -50,13 +66,15 @@ const removeFromCart = async (req, res) => {
       where: {
         userId: userId,
         productId: productId,
-      }
+      },
     });
   } else {
-    req.session.cart = req.session.cart.filter(item => item.productId !== productId);
+    req.session.cart = req.session.cart.filter(
+      (item) => item.productId !== productId
+    );
   }
 
-  res.redirect('/cart');
+  res.redirect("/cart");
 };
 
 module.exports = { addToCart, viewCart, removeFromCart };
